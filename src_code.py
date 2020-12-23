82879b587ba126a5181afc3a37ecc1d12051fbd0
CriticalHunter <sambit1suranjan@gmail.com>
2020-12-23 10:57:39 +0530

    Adding one month worth changes

--- src_code.py
+++ src_code.py
@@ -1,20 +1,97 @@
 #https://developers.google.com/youtube/v3/docs
 from googleapiclient.discovery import build
-from datetime import datetime
+from datetime import datetime, timedelta
+import sqlite3, time
+from bs4 import BeautifulSoup
 
-import sqlite3
 global count1
 global api_key, youtube
-
 count1 = 0
 
+'''
+To run any function(s), first run get_api_key, to start the Youtube API
+'''
+def create_new():
+    conn = sqlite3.connect('youtube.db')              
+    cur = conn.cursor() 
 
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS tb_channels (
+        Channel_ID TEXT PRIMARY KEY,
+        Channel_title TEXT,
+        Published_At TEXT NOT NULL,
+        Country TEXT,
+        View_Count INTEGER,
+        Subscriber_Count INTEGER,
+        Video_Count INTEGER,
+        Playlist_Count INTEGER
+    )
+
+        """)
+
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS tb_playlists(
+        Playlist_ID TEXT PRIMARY KEY,
+        Playlist_title TEXT,
+        Channel_ID TEXT NOT NULL,
+        Channel_Title TEXT NOT NULL,
+        Published_At TEXT NOT NULL,
+        Item_Count INTEGER,
+        Playlist_Seconds INTEGER,
+        Playlist_Duration TEXT,
+        Is_Seen INTEGER,
+        Worth INTEGER,
+        FOREIGN KEY (Channel_ID)
+        REFERENCES tb_channels (Channel_ID)
+    )
+    """)
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS tb_videos (
+        Video_ID TEXT PRIMARY KEY,
+        Video_title TEXT,
+        Is_Seen INTEGER,
+        Worth INTEGER,
+        Upload_playlistId TEXT,
+        Playlist_ID TEXT,
+        Published_At TEXT NOT NULL,
+        epoch REAL NOT NULL,
+        Channel_ID TEXT NOT NULL,
+        Channel_Title TEXT NOT NULL,
+        View_Count INTEGER,
+        Like_Count INTEGER,
+        Dislike_Count INTEGER,
+        Upvote_Ratio REAL,
+        Comment_Count INTEGER,
+        Duration TEXT,
+        video_seconds INTEGER,
+        Is_Licensed INTEGER,
+        Is_Deleted INTEGER,
+        FOREIGN KEY (Channel_ID)
+        REFERENCES tb_channels (Channel_ID),
+        FOREIGN KEY (Playlist_ID)
+        REFERENCES tb_playlists (Playlist_ID)
+    )
+    """)
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS video_history (
+        Video_ID TEXT NOT NULL,
+        Watched_at TEXT ,
+        epoch REAL NOT NULL,
+        Is_in_Main INTEGER,
+        PRIMARY KEY ( Video_ID, epoch),
+        FOREIGN KEY (Video_ID)
+        REFERENCES tb_videos (Video_ID)
+    )
+
+        """)
+        
 def get_api_key(key):
     
     global api_key,youtube
     api_key = key
     youtube = build('youtube','v3',developerKey=api_key)
 
+get_api_key('AIzaSyBSumleKJ1eLSsTcMxXH2szgB_OCOXnslg')
 
 
 def oldest_videos_on_a_topic(topic):
@@ -32,6 +109,11 @@ def oldest_videos_on_a_topic(topic):
         print(item['snippet']['title'], item['snippet']['publishedAt'], item['id']['videoId'])
     #print(res)
 
+
+
+'''
+Increasingly, searching by channel name returns None. Prefer search by ID
+'''
 def get_channel_id(ch_name):
     global youtube
     request = youtube.channels().list(
@@ -48,7 +130,7 @@ def get_channel_id(ch_name):
             sub_count = str(sub_count / 1000)
             sub_count = sub_count + 'K Subscribers'
         else:
-            sub_count = sub_count + ' Subscribers'
+            sub_count = str(sub_count) + ' Subscribers'
         ch_id = response['items'][0]['id']
         
         print(" ")
@@ -61,16 +143,18 @@ def get_channel_id(ch_name):
 
 
 
+
 def get_videos_stats(video_ids,flag,playlistID = None):
     global youtube
     conn = sqlite3.connect('youtube.db')              
     cur = conn.cursor()
     count1 = 0
-
     stats = []
+    tot_len = 0
+
     for i in range(0, len(video_ids), 50):
         res = youtube.videos().list(id=','.join(video_ids[i:i+50]),
-                                   part='snippet,statistics').execute()
+                                   part='snippet,statistics,contentDetails').execute()
         stats += res['items']
     
 
@@ -87,12 +171,15 @@ def get_videos_stats(video_ids,flag,playlistID = None):
             Playlist_Id = playlistID                                    # When call is grom a playlist
             
         Published_At = video['snippet']['publishedAt']
+        date_format = "%Y-%m-%dT%H:%M:%SZ" 
+        epoch = float(time.mktime(time.strptime(Published_At, date_format)))
         Channel_Id = video['snippet']['channelId']
         Channel_Title = video['snippet']['channelTitle']
         try:
             View_Count = video['statistics']['viewCount']
         except:
             View_Count = 0
+            flag = 2
         try:
             Like_Count = video['statistics']['likeCount']
         except:
@@ -102,23 +189,110 @@ def get_videos_stats(video_ids,flag,playlistID = None):
         except:
             Dislike_Count = 0
         try:
-            Favorite_Count = video['statistics']['favoriteCount']
+            Upvote_Ratio = (int(Like_Count)/(int(Like_Count)+(int(Dislike_Count))))*100
         except:
-            Favorite_Count = 0
+            Upvote_Ratio = 0
         try:
             Comment_Count = video['statistics']['commentCount']
         except:            
             Comment_Count = 0
+        try:
+            Duration = str(video['contentDetails']['duration'])
+            Duration = Duration.replace('PT','')
+            hh=mm=ss = '00'
+            if Duration.find('H') != -1:
+                hh = Duration.split('H')[0]
+                temp = hh+'H'
+                if len(hh) == 1:
+                    hh = '0'+hh
+                Duration = Duration.replace(temp,'')
+            if Duration.find('M') != -1:
+                mm = Duration.split('M')[0]
+                temp = mm+'M'
+                if len(mm) == 1:
+                    mm = '0'+mm
+                Duration = Duration.replace(temp,'')
+            if Duration.find('S') != -1:
+                ss = Duration.split('S')[0]
+                if len(ss) == 1:
+                    ss = '0'+ss
+            Duration = (hh+':'+mm+':'+ss)
+            video_seconds = timedelta(hours = int(hh),
+                            minutes= int(mm),
+                            seconds= int(ss)).total_seconds()
+            if playlistID is not None:
+                tot_len += video_seconds
+        except:            
+            Duration = '0'
+            video_seconds = 0
+            flag = 2
             
-        params = (Video_id,Video_title,Upload_playlistId,Playlist_Id,Published_At,Channel_Id,Channel_Title,View_Count,Like_Count,Dislike_Count,Favorite_Count,Comment_Count)
+        try:
+            Is_Licensed = video['contentDetails']['licensedContent']
+        except:            
+            Is_Licensed = 0 
+        Is_Seen = 0                     # 0 = not seen    1 = seen
+        Worth = 0                       # 0 = not rated , ratings = 1(not worth saving)/2(worth saving)
+        Is_Deleted = 0
+        params = (Video_id,Video_title,Is_Seen,Worth,Upload_playlistId,Playlist_Id,Published_At,epoch,Channel_Id,Channel_Title,View_Count,Like_Count,Dislike_Count,Upvote_Ratio,Comment_Count,Duration,video_seconds,Is_Licensed,Is_Deleted)
         if flag == 1:
-            cur.execute("INSERT OR REPLACE INTO tb_videos VALUES (?, ?, ?, ?, ?, ?, ?, ? ,? ,? ,? ,? )", params)
+            cur.execute("INSERT OR REPLACE INTO tb_videos VALUES (?, ?, ?, ?, ?, ?, ? ,? ,? ,? ,? ,? , ?, ?, ?, ?, ?, ?, ?)", params)
         else:
-            cur.execute("INSERT OR IGNORE INTO tb_videos VALUES (?, ?, ?, ?, ?, ?, ?, ? ,? ,? ,? ,? )", params)
+            cur.execute("INSERT OR IGNORE INTO tb_videos VALUES (?, ?, ?, ?, ?, ?, ? ,? ,? ,? ,? ,? , ?, ?, ?, ?, ?, ?, ?)", params)
 
     conn.commit()                                               # Push the data into database
     conn.close()
+    if tot_len > 0:
+        return tot_len
 
+
+def get_channel_playlists(channel_id):
+    global youtube
+    conn = sqlite3.connect('youtube.db')              
+    cur = conn.cursor()
+    
+    playlists = []
+    playlist_ids = []
+    next_page_token = None
+    
+    while 1:
+        res = youtube.playlists().list( part="snippet,contentDetails",
+                                        channelId=channel_id,
+                                        pageToken=next_page_token,
+                                        maxResults=50
+                                    ).execute()
+        playlists += res['items']
+        next_page_token = res.get('nextPageToken')
+        
+
+        for playlist in playlists:
+
+            Playlist_ID = playlist['id']                    ;   playlist_ids.append(Playlist_ID) 
+            Playlist_title = playlist['snippet']['title']
+            Channel_Id = playlist['snippet']['channelId']
+            Channel_Title = playlist['snippet']['channelTitle']
+            Published_At = playlist['snippet']['publishedAt']
+            Item_Count = playlist['contentDetails']['itemCount']
+            Playlist_Seconds = 0
+            Playlist_Duration = '0'
+            Is_Seen = 0                     # 0 = not seen    1 = seen
+            Worth = 0                       # 0 = not rated , ratings = 1(not worth saving)/2(worth saving)
+            params = (Playlist_ID,Playlist_title,Channel_Id,Channel_Title,Published_At,Item_Count,Playlist_Seconds,Playlist_Duration,Is_Seen,Worth)
+            cur.execute("INSERT OR REPLACE INTO tb_playlists VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", params)
+        if next_page_token is None:
+            break
+    
+    playlist_ids = set(playlist_ids)
+    playlist_ids = list(playlist_ids)
+    count = len(playlist_ids)
+    cur.execute("UPDATE tb_channels SET Playlist_Count = ? WHERE Channel_ID = ? ",(count,Channel_Id))
+
+    conn.commit()                                               # Push the data into database
+    conn.close()
+    
+    return playlist_ids
+
+''' get_channel_details() and  entire_channel(ch_id) use  get_channel_playlists '''
 def get_channel_details(channel_id):
     global youtube
     conn = sqlite3.connect('youtube.db')              
@@ -139,17 +313,24 @@ def get_channel_details(channel_id):
     except:
         Country = None
     View_Count = request['items'][0]['statistics']['viewCount']
-    Comment_Count = request['items'][0]['statistics']['commentCount']
+
     Subscriber_Count = request['items'][0]['statistics']['subscriberCount']
     Video_Count = request['items'][0]['statistics']['videoCount']
 
-    params = (Channel_Id,Channel_title,Published_At,Country,View_Count,Comment_Count,Subscriber_Count,Video_Count)
+    params = (Channel_Id,Channel_title,Published_At,Country,View_Count,Subscriber_Count,Video_Count)
 
-    cur.execute("INSERT OR REPLACE INTO tb_channels VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0)", params)
+    cur.execute("INSERT OR REPLACE INTO tb_channels VALUES (?, ?, ?, ?, ?, ?, ?, 0)", params)
     
     conn.commit()                                               # Push the data into database
     conn.close()
 
+    get_channel_playlists(Channel_Id)
+
+
+
+
+
+''' get_playlist_videos(playlistID) takes only a single Playlist ID as string  '''
 def get_playlist_videos(playlistID):
     global youtube
     conn = sqlite3.connect('youtube.db')              
@@ -176,59 +357,23 @@ def get_playlist_videos(playlistID):
             Video_id = video['snippet']['resourceId']['videoId'];   video_IDS.append(Video_id)
 
             params = (Video_id,"","","","","","")
-            cur.execute("INSERT OR IGNORE INTO tb_videos VALUES (?, ?, ?, ?, ?, ?, ?, 0,0,0,0,0)", params)    
+            cur.execute("INSERT OR IGNORE INTO tb_videos VALUES (?, ?, ?, ?, ?, ?, ?, 0,0,0,0,0,'',0,0,0,0)", params)    
 
         
     print('Videos in this playlist =',len(video_IDS))
     conn.commit()                                               # Push the data into database
     conn.close()
     
-    get_videos_stats(video_IDS,1,playlistID)
-
-    
-
-def get_channel_playlists(channel_id):
-    global youtube
+    Playlist_Seconds = get_videos_stats(video_IDS,1,playlistID)
+    Playlist_Duration = str(timedelta(seconds = Playlist_Seconds))
     conn = sqlite3.connect('youtube.db')              
     cur = conn.cursor()
-    
-    playlists = []
-    playlist_ids = []
-    count = 0
-    next_page_token = None
-    
-    while 1:
-        res = youtube.playlists().list( part="snippet,contentDetails",
-                                        channelId=channel_id,
-                                        pageToken=next_page_token,
-                                        maxResults=50
-                                    ).execute()
-        playlists += res['items']
-        next_page_token = res.get('nextPageToken')
-        
-
-        for playlist in playlists:
-            count += 1
-
-            Playlist_ID = playlist['id']                    ;   playlist_ids.append(Playlist_ID) 
-            Playlist_title = playlist['snippet']['title']
-            Channel_Id = playlist['snippet']['channelId']
-            Channel_Title = playlist['snippet']['channelTitle']
-            Published_At = playlist['snippet']['publishedAt']
-            Item_Count = playlist['contentDetails']['itemCount']
-            params = (Playlist_ID,Playlist_title,Channel_Id,Channel_Title,Published_At,Item_Count)
-            cur.execute("INSERT OR REPLACE INTO tb_playlists VALUES (?, ?, ?, ?, ?, ?)", params)
-        if next_page_token is None:
-            break
-    
-    cur.execute("UPDATE tb_channels SET Playlist_Count = ? WHERE Channel_ID = ? ",(count,Channel_Id))
-
-    
-
-
+    cur.execute("UPDATE tb_playlists SET Playlist_Seconds = ? WHERE playlist_ID = ? ",(Playlist_Seconds,playlistID))
+    cur.execute("UPDATE tb_playlists SET Playlist_Duration = ? WHERE playlist_ID = ? ",(Playlist_Duration,playlistID))
     conn.commit()                                               # Push the data into database
     conn.close()
-    return playlist_ids
+    
+
 
 
 
@@ -272,7 +417,55 @@ def get_channel_videos(channel_id):
     print('\nParsing ',len(new_video_ids),' videos, which are not in any playlist')
     get_videos_stats(video_ids,flag=0)
 
+def load_history():
+    count_loc_prog = 0
+    with open("takeout/history/watch-history.html",encoding='utf-8') as fp:
+        conn = sqlite3.connect('youtube.db')              
+        cur = conn.cursor()
 
+        soup = BeautifulSoup(fp,'lxml')
+        soup = soup.body
+
+        videos = soup.find_all("div", {"class": "content-cell mdl-cell mdl-cell--6-col mdl-typography--body-1"})
+
+        print(len(videos))
+        
+        for video in videos:
+            count_loc_prog += 1
+            if count_loc_prog % 500 == 0:
+                print(count_loc_prog)
+            tags = video.find_all('a')
+            # try:
+
+            if tags == []:
+                continue
+            
+            V_link = tags[0].get('href')
+            V_link = V_link.split('=')[-1]
+            br_tags = video.find_all('br')
+            for tag in br_tags:
+                watched_at = str(tag.next_sibling)
+                if watched_at[-3:-1] == 'IS':
+                    final_time = (watched_at)
+                    temp = final_time.replace('IST','+0530')
+                    epoch = time.mktime(time.strptime(temp, "%b %d, %Y, %H:%M:%S %p %z"))
+            cur.execute("INSERT OR IGNORE INTO video_history VALUES (?,?,?,?)", (V_link,final_time,epoch,0))
+               
+            # except:
+
+            #     continue
+            
+
+        conn.commit()                                               # Push the data into database
+        conn.close()
+
+    def freq():
+        conn = sqlite3.connect('youtube.db')              
+        cur = conn.cursor()
+
+        
+
+load_history()
 
 def entire_channel(ch_id):
     get_channel_details(ch_id)
@@ -287,5 +480,7 @@ def entire_channel(ch_id):
         count1 = 0
     get_channel_videos(ch_id)
 
+# entire_channel('UCJQJ4GjTiq5lmn8czf8oo0Q')
 def just_playlist(playlist_id):
     get_playlist_videos(playlist_id)
+
